3f5f3019bc60059b2d7691f86228b21738ad2474
qms95 <140948284+qms95@users.noreply.github.com>
2021-11-07 15:15:30 +0000

    Project reorganization

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from flask import Flask, render_template, redirect
 from generate_poem import PoemGenerator
 app = Flask(__name__)
 
-generator = PoemGenerator(
+generator = PoemGenerator(None)
+
+
+@
