98cd0a37e4fd613d65123df6fb5e134bd2a95cce
qms95 <140948284+qms95@users.noreply.github.com>
2021-12-07 19:11:06 +0000

    net: avoid NULL deref in inet_ctl_sock_destroy()

--- count_syllables.py
+++ count_syllables.py
@@ -8,3 +8,13 @@ cmu = cmudict.dict()
 
 
 def count_syllables(word):
+    lower_word = word.lower()
+    if lower_word in cmu:
+        return max([len([y for y in x if y[-1] in string.digits])
+                    for x in cmu[lower_word]])
+    else:
+        return sylco(word)
+ 
+
+ # by M. Emre Aydin from http://eayd.in/?p=232
+def sylco(word) :
