1a4917929186e39be4ede9735af976181a66c07d
qms95 <140948284+qms95@users.noreply.github.com>
2021-12-10 13:17:15 +0000

    Adds screenshot

--- count_syllables.py
+++ count_syllables.py
@@ -44,3 +44,13 @@ def sylco(word) :
     # if it has only 1 vowel or 1 set of consecutive vowels, discard. (like "speed", "fled" etc.)
  
     if word[-2:] == "es" or word[-2:] == "ed" :
+        doubleAndtripple_1 = len(re.findall(r'[eaoui][eaoui]',word))
+        if doubleAndtripple_1 > 1 or len(re.findall(r'[eaoui][^eaoui]',word)) > 1 :
+            if word[-3:] == "ted" or word[-3:] == "tes" or word[-3:] == "ses" or word[-3:] == "ied" or word[-3:] == "ies" :
+                pass
+            else :
+                disc+=1
+ 
+    #3) discard trailing "e", except where ending is "le"  
+ 
+    le_except = ['whole','mobile','pole','male','female','hale','pale','tale','sale','aisle','whale','while']
