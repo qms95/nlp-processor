97b183d67d41a3e17c66c438eb697fa40d8514a0
qms95 <140948284+qms95@users.noreply.github.com>
2022-04-01 23:02:42 +0000

    1.21

--- generate_poem.py
+++ generate_poem.py
@@ -8,3 +8,14 @@ import csv
 import inflect
 from count_syllables import count_syllables
 
+
+class PoemGenerator(object):
+    def __init__(self, corpus='buzzfeed_facebook_statues.csv'):
+        self.only_punctuation = re.compile(r'[^\w\s]+$')
+        self.spaces_and_punctuation = re.compile(r"[\w']+|[.,!?;]")
+        self.sents = []
+        self.words = []
+        self.all_words = []
+        self.inflect_engine = inflect.engine()
+        self.read_corpus(corpus)
+        self.bigrams = list(nltk.bigrams(self.words))
