a22843b531c5a183e97385b86865a19358028ad9
qms95 <140948284+qms95@users.noreply.github.com>
2022-04-19 21:51:53 +0000

    Fix installation

--- generate_random.py
+++ generate_random.py
@@ -1,3 +1,13 @@
 
 import nltk
 import random
+
+TEXT = nltk.corpus.genesis.words('english-kjv.txt')
+
+
+def main():
+    bigrams = nltk.bigrams(TEXT)
+    cfdist = nltk.ConditionalFreqDist(bigrams)
+    word = random.choice(bigrams)[0]
+    for i in range(15):
+        print word,
