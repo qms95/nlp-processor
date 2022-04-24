6e7797e4f90cb77b6eb340ae55c5b9767c335bf6
qms95 <140948284+qms95@users.noreply.github.com>
2022-04-24 16:38:08 +0000

    Fixed JavaDoc

--- generate_random_from_file.py
+++ generate_random_from_file.py
@@ -3,3 +3,9 @@ import nltk
 import random
 import string
 import sys
+
+
+def main(text):
+    bigrams = list(nltk.bigrams(
+        [token for token in nltk.word_tokenize(text.decode('utf8'))
+         if set(token).difference(set(string.punctuation))]))
