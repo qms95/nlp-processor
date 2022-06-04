997d3d8227966ab90a9464727e49abb66ba86e02
qms95 <140948284+qms95@users.noreply.github.com>
2022-06-04 20:13:01 +0000

    ath9k_htc: configure bssid on ASSOC/IBSS change

--- markov_matrix.py
+++ markov_matrix.py
@@ -6,3 +6,11 @@ the conditional word.
 
 This was an interesting idea, but ultimately not that useful since the resulting numpy array is significantly larger
 than just storing the CFD in a python dictionary. There might be some crazy linear algebra I could run to compress this
+array to make it less sparse. But, I would need to use the same N words for all corpora and I think that the resulting
+compressed arrays would only be really useful for comparing with each other to find things like "closeness" between two
+corpora as defined by the probabilities that some words follow other words in the text. Also, using the same N words
+across all corpora is less awesome because you will miss out on the unique words (names, proper nouns, etc.) present in
+only some corpora.
+"""
+import codecs
+import sys
