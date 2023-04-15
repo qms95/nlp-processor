08f52034845fb01f52f32d9461c175820692c5b3
qms95 <140948284+qms95@users.noreply.github.com>
2023-04-15 21:02:47 +0000

    more ignore

--- syntax_aware_generate.py
+++ syntax_aware_generate.py
@@ -3,3 +3,18 @@ import nltk
 import operator
 import os
 import pickle
+import random
+import re
+import codecs
+import sys
+from nltk.tree import Tree
+from collections import defaultdict
+from tqdm import tqdm
+from stat_parser import Parser
+
+syntaxes = defaultdict(set)
+SYNTAXES_FILE = 'syntaxes.p'
+CFDS_FILE = 'cfds.p'
+
+
+def tree_hash(self):
