d3b88e8272ee3dfa5ef010e3e712410fb012e616
qms95 <140948284+qms95@users.noreply.github.com>
2023-05-12 07:19:20 +0000

    skin

--- syntax_gen.py
+++ syntax_gen.py
@@ -8,3 +8,10 @@ import spacy
 
 TEMPLATE_CORPUS = 'austencorpus'
 CONTENT_CORPUS = 'lovecraftcorpus'
+
+print('Loading spaCy model... ', end='')
+nlp = spacy.load('en_core_web_lg')
+print('Done')
+
+
+def load_text_files(dirname):
